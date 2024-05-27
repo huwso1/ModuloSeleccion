8ee017ba0ad1a0046a9de53e90974e6e4ddc92f8
Tomás Cedeño <tomasj.cedenop@gmail.com>
2024-05-27 01:01:03 -0500

    [UPDATED] retornar nombre responsable en requerimientos

--- back/src/services/requirementService.js
+++ back/src/services/requirementService.js
@@ -55,19 +55,20 @@ async function getRequirementsByGeneralAnalystId(analystId) {
     connection = await oracledb.getConnection();
 
     const query = `
-      SELECT R.*, 
-             CASE 
-               WHEN (SELECT COUNT(PR.consproceso)
-                     FROM ProcesoRequerimiento PR
-                     WHERE R.consecReque = PR.consecReque) = 0
-               THEN 1
-               ELSE (SELECT COUNT(PR.consproceso)
-                     FROM ProcesoRequerimiento PR
-                     WHERE R.consecReque = PR.consecReque)
-             END AS fase
-      FROM Requerimiento R
-      JOIN Empleado E ON R.codEmpleado2 = E.codEmpleado
-      WHERE E.codEmpleado = :analystId
+    SELECT R.*, Res.nomEmpleado||' '||Res.apellEmpleado responsable,
+        CASE 
+          WHEN (SELECT COUNT(PR.consproceso)
+                FROM ProcesoRequerimiento PR
+                WHERE R.consecReque = PR.consecReque) = 0
+          THEN 1
+          ELSE (SELECT COUNT(PR.consproceso)
+                FROM ProcesoRequerimiento PR
+                WHERE R.consecReque = PR.consecReque)
+        END AS fase
+    FROM Requerimiento R
+    JOIN Empleado E ON R.codEmpleado2 = E.codEmpleado
+    JOIN Empleado Res ON Res.codEmpleado = E.codEmpleado
+    WHERE E.codEmpleado = :analystId
     `;
 
     const result = await connection.execute(query, { analystId }, { outFormat: oracledb.OUT_FORMAT_OBJECT });
